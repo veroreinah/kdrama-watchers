20a7b4740fe86cde857921c1cb74d74417493ea7
Verónica Reina Hernández <veroreinah@gmail.com>
2021-01-01 17:35:35 +0100

    Firebase setup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase/app'
+import "firebase/auth";
+import "firebase/firestore";
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyBAKjOAXWCdT9wSH_-Zx4mW0rWK7QguRgM",
+  projectId: "kdrama-watchers",
+};
+firebase.initializeApp(firebaseConfig);
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+});
