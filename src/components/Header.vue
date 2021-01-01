22a15ae53727d8e9eb8e17ea4ec1f0243a5e2fcf
Verónica Reina Hernández <veroreinah@gmail.com>
2021-01-01 18:59:24 +0100

    Create Header component. Custom Vuetify theme

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar
+    fixed
+    color="primary"
+    dark
+    shrink-on-scroll
+    :src="$vuetify.breakpoint.xs ? mobileBg : desktopBg"
+    height="128"
+    min-height="56"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+      ></v-img>
+    </template>
+
+    <v-app-bar-title>
+      <div>K-drama watchers</div>
+      <div class="text-subtitle-1 font-weight-thin">We watch kdramas</div>
+    </v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import mobileBg from '@/assets/img/header-bg-mobile.jpg';
+import desktopBg from '@/assets/img/header-bg-desktop.jpg';
+
+export default {
+  name: "Header",
+  data: () => ({
+    mobileBg,
+    desktopBg,
+  }),
+};
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#6A76AB", // Blue yonder
+        secondary: "#B17EB9", // African violet
+        accent: "#F186AC", // Tickle me pink
+        error: '#EE6352', // Fire opal
+        info: '#08B2E3', // Cyan process
+        success: '#57A773', // Forest green Crayola
+        warning: '#FA824C', // Mandarin
+      },
+    },
+  },
 });
