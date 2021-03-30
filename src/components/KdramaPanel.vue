7c2c039c30b6c6671b37ea43b887114b0046447c
Verónica Reina Hernández <veroreinah@gmail.com>
2021-03-30 19:44:50 +0200

    Move kdramas from one list to another

--- src/components/KdramaPanel.vue
+++ src/components/KdramaPanel.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
     <v-expansion-panel-header hide-actions class="pa-0">
-      <KdramaCard :kdrama="kdrama" small hide-actions />
+      <div class="d-flex align-sm-center justify-space-between">
+        <KdramaCard :kdrama="kdrama" small hide-actions />
+
+        <div v-if="actions[kdrama.list]" class="pa-4 d-flex flex-column d-sm-block">
+          <v-btn
+            v-for="action in actions[kdrama.list]"
+            :key="action.action"
+            fab
+            depressed
+            x-small
+            color="secondary"
+            :loading="currentAction === action.action && loading"
+            :disabled="loading"
+            @click.stop="moveKdrama(action.action)"
+            class="ml-1 ml-sm-2 mb-1 mb-sm-0"
+          >
+            <v-icon>{{ action.icon }}</v-icon>
+          </v-btn>
+        </div>
+      </div>
     </v-expansion-panel-header>
 
     <v-expansion-panel-content class="pt-5">
@@ -9,13 +28,13 @@
 
       <template v-for="item in kdramaData">
         <template v-if="kdrama[item.key]">
-          <div :key="item.key" class="mb-1">
+          <div :key="item.key" class="mb-4">
             <h3>{{ item.label }}</h3>
             <div v-html="getFormattedText(kdrama[item.key])"></div>
           </div>
         </template>
       </template>
-      <div v-if="kdrama.trivia" class="mb-1">
+      <div v-if="kdrama.trivia" class="mb-4">
         <h3>Curiosidades</h3>
         <TriviaList :data="kdrama.trivia" />
       </div>
@@ -27,6 +46,8 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/firestore";
 import KdramaCard from '@/components/KdramaCard';
 import KdramaDates from '@/components/KdramaDates';
 import TriviaList from '@/components/TriviaList';
@@ -40,6 +61,9 @@ export default {
   },
   data: () => ({
     mobileBg,
+    db: undefined,
+    loading: false,
+    currentAction: undefined,
     kdramaData: [
       {
         label: 'Episodios',
@@ -49,7 +73,33 @@ export default {
         label: 'Sinopsis',
         key: 'synopsis',
       },
-    ]
+    ],
+    actions: {
+      wishlist: [
+        {
+          action: 'currently-watching',
+          icon: 'mdi-eye-plus',
+        },
+        {
+          action: 'already-watched',
+          icon: 'mdi-eye-check',
+        },
+        {
+          action: 'abandoned',
+          icon: 'mdi-heart-off',
+        },
+      ],
+      'currently-watching': [
+        {
+          action: 'already-watched',
+          icon: 'mdi-eye-check',
+        },
+        {
+          action: 'abandoned',
+          icon: 'mdi-heart-off',
+        },
+      ],
+    },
   }),
   components: {
     KdramaCard,
@@ -70,6 +120,43 @@ export default {
     getFormattedText(text) {
       return text.replaceAll('[[', '<strong>').replaceAll(']]', '</strong>');
     },
+    moveKdrama(list) {
+      this.loading = true;
+      this.currentAction = list;
+      let toSave = { ...this.kdrama, list };
+
+      if (list === 'currently-watching') {
+        toSave = { ...toSave, dateStart: (new Date()).toISOString().substr(0, 10) };
+      } else if (list === 'already-watched' || list === 'abandoned') {
+        toSave = { ...toSave, dateEnd: (new Date()).toISOString().substr(0, 10) };
+      }
+
+      this.db.collection('kdramas').doc(this.kdrama.id).set(toSave)
+        .then(() => {
+          this.setSnackbar({
+            msg: `El kdrama "${this.kdrama.title}" se ha actualizado correctamente.`,
+            color: "success",
+            timeout: 5000
+          });
+          
+          this.$emit('updateList');
+        })
+        .catch(error => {
+          console.error(error);
+          this.setSnackbar({
+            msg: "Ha habido un error al mover el kdrama a la lista seleccionada.",
+            color: "error",
+            timeout: 10000
+          });
+        })
+        .finally(() => {
+          this.loading = false;
+          this.currentAction = undefined;
+        });
+    },
+  },
+  created() {
+    this.db = firebase.firestore();
   },
 }
 </script>
